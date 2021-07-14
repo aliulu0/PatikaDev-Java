1ca49e24a8454801b910172851bafb57a722b13a
Ali Ulu <67802869+aliulu0@users.noreply.github.com>
2021-07-14 23:37:25 +0300

    Update Main.java

--- Not Ortalaması Hesaplayan Program/Main.java
+++ Not Ortalaması Hesaplayan Program/Main.java
@@ -1,21 +1,36 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        double tutar,kdvORan,kdvTutar,kdvLiTutar;
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Ücret tutarı giriniz: ");
-        tutar = input.nextDouble();
-
-        kdvORan = tutar > 0 && tutar < 1000 ? 0.18 : 0.08;
-        kdvTutar = tutar * kdvORan;
-        kdvLiTutar = tutar + kdvTutar;
-
-
-        System.out.println("KDV'siz tutar : "+tutar);
-        System.out.println("KDV Oranı : "+kdvORan);
-        System.out.println("KDV'li tutar : "+kdvLiTutar);
-        System.out.println("KDV tutar : "+kdvTutar);
+        int matematik,fizik,turkce,kimya,tarih,muzik;
+		
+		Scanner input  = new Scanner(System.in);
+		
+		System.out.println("Matematik notunuz : ");
+		matematik = input.nextInt();
+		
+		System.out.println("Fizik notunuz : ");
+		fizik = input.nextInt();
+		
+		System.out.println("Türkçe notunuz : ");
+		turkce = input.nextInt();
+		
+		
+		System.out.println("Kimya notunuz : ");
+		kimya = input.nextInt();
+		
+		
+		System.out.println("Tarih notunuz : ");
+		tarih = input.nextInt();
+		
+		
+		System.out.println("Müzik notunuz : ");
+		muzik = input.nextInt();
+		
+		double sonuc = (matematik+fizik+turkce+kimya+tarih+muzik)/6.0;
+		System.out.println("Ortalamanız : " + sonuc);
+		
+		boolean gectiKaldı =sonuc > 60 ;
+		System.out.println(gectiKaldı == true ? "Sınıfı geçti":"Sınıfta kaldı");
 
 
     }
