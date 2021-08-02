727fc2283e541f88e37c7eb2e964c7db3e90bdff
Ali Ulu <67802869+aliulu0@users.noreply.github.com>
2021-08-02 20:03:07 +0300

    Delete Main.java

--- PATIKA-JAVA/Maaş Hesaplayıcı/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Employee employee = new Employee("Kemal", 2000.0, 45, 1985);
-        employee.toString(employee);
-
-    }
-
-}
